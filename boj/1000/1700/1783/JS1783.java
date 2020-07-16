0c962ff7bc559c5af70248da8d96c000ae963d26
kjs <46150254+JsKim4@users.noreply.github.com>
2020-07-16 14:31:47 +0900

    병든나이트 - 김준섭

---
+++ boj/1000/1700/1783/JS1783.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class JS1783 {
+
+
+    public static void main(String[] args) {
+        new JS1783().solve();
+    }
+
+    private void solve() {
+        Scanner sc = new Scanner(System.in);
+
+        int height = sc.nextInt();
+        int width = sc.nextInt();
+
+        int answer = 0;
+        if (height == 1) {
+            answer = 1;
+        } else if (height == 2) {
+            answer = (width + 1) / 2;
+            answer = answer > 4 ? 4 : answer;
+        }else{
+            if(width < 5){
+                answer = width;
+            }
+            else if(width == 5){
+                answer = 4;
+            }else{
+                answer = width-2;
+            }
+        }
+        System.out.println(answer);
+
+    }
+}
